e90aa222fcc0fa7a9ba95393101f6f0e6768092e
kunj-21 <107642217+kunj-21@users.noreply.github.com>
2024-01-29 14:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var dateDiffInDays = function (date1, date2) {
    
 };
 
-// // Do not change the code below.
-// const dateOne = prompt("Enter Start Date.");
-// const dateTwo = prompt("Enter End Date.");
+ Do not change the code below.
+ const dateOne = prompt("Enter Start Date.");
+ const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
