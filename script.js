7a2b33a173f3286464a455135a27a8b41c5c4175
kunj-21 <107642217+kunj-21@users.noreply.github.com>
2024-01-29 14:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  const timeDifference = date1 - date2;
+
+    const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+	if(date1 === date2){
+		return 0;
+	}else if(date2-date1){
+		return -1;
+	}else{
+		c
+	}
+
+    return Math.floor(daysDifferene);
 };
 
-// Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+// // Do not change the code below.
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
